ae842f1424fe9af6cbedc7b63b8ddc618eb1fb80
Hamed Akbarzadeh <h.akbarzadh@gmail.com>
2025-02-15 23:44:39 +0330

    add & feat : add error handle (exceptions) in project and current add badRequest class and httpExecption class

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,15 +1,27 @@
-import { Request, response, Response } from "express"
+import { NextFunction, Request, response, Response } from "express"
 import { prisma } from "../prismaConif/client";
 import bcrypt from "bcrypt";
-export async function register(req: Request, res: Response) {
+import { BadRequestException } from "../exceptions/badRequest";
+import { ErrorCode } from "../exceptions/httpException";
+export async function register(req: Request, res: Response, next: NextFunction) {
+    let user = await prisma.user.findFirst({
+        where: {
+            email: req.body.email
+        }
+    });
+    console.log("current User : " + user);
+
+    if (user) {
+        next(new BadRequestException("User already exists", ErrorCode.USER_ALREADY_EXISTS));
+    }
     let password = req.body.password;
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassowrd = await bcrypt.hash(password, salt);
-    const user = await prisma.user.create({
+    user = await prisma.user.create({
         data: {
             name: req.body.name,
-            email: req.body.name,
+            email: req.body.email,
             password: hashedPassowrd
         },
     });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import { PORT } from "./secret";
 import apiRouter from "./routes/apiRoute";
+import { errorMiddleware } from "./middlewares/errorsMiddleware";
 
 const app: Express = express();
 
 app.use(express.json());
 
-
 app.use("/api", apiRouter);
+
+app.use(errorMiddleware)
 
 app.listen(PORT, () => console.log("Listen on port " + PORT));
